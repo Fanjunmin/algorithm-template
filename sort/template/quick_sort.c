5c745a47e3e76244ce78f3ac647ca82e9efae2af
fanjunmin <junminfan@126.com>
2021-04-26 08:56:54 +0800

    add sort folder

---
+++ sort/template/quick_sort.c
@@ -0,0 +1,15 @@
+#define ff_sort(x, y) do {  \
+    x ^= y, y ^= x, x ^= y; \
+} while(0)
+
+int quick_sort(int q[], int l, int r)
+{
+    if (l >= r) return;
+    int x = q[l + r >> 1], i = l - 1, j = r + 1;
+    while (i < j) {
+        while (q[++i] < x);
+        while (q[--j] > x);
+        if (i < j) ff_sort(q[i], q[j]);
+    } 
+    quick_sort(q, l, j), quick_sort(q, j + 1, r);
+}
