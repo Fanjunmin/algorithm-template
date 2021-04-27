3616e78800989d6e0aa8e7c55db07ca4a67e0242
fanjunmin <junminfan@126.com>
2021-04-27 08:45:16 +0800

    add .gitignore; add mul & div high accuracy template

--- high_accuracy/template/high_accuracy.cpp
+++ high_accuracy/template/high_accuracy.cpp
@@ -1,4 +1,5 @@
 #include <vector>
+#include <algorithm>
 /*
     123456789
     A = {9,8,7,6,5,4,3,2,1}
@@ -44,6 +45,40 @@ std::vector<int> sub(const std::vector<int> &A, const std::vector<int> &B)
         if (t < 0) t = -1;
         else t = 0;
     }
+    while (C.size() > 1 && C[C.size() - 1] == 0) C.pop_back();
+    return C;
+}
+
+/* 
+    high accuracy mul
+*/
+std::vector<int> mul(const std::vector<int> &A, const int &b)
+{
+    std::vector<int> C;
+    int t = 0;
+    for (int i = 0; i < A.size() || t; ++i) {
+        if (i < A.size()) t += A[i] * b;
+        C.push_back(t % 10);
+        t /= 10;
+    }
+    while (C.size() > 1 && C[C.size() - 1] == 0) C.pop_back();
+    return C;
+}
+
+/* 
+    high accuracy div
+*/
+std::vector<int> div(const std::vector<int> &A, const int &b, int &r)
+{
+    std::vector<int> C;
+    r = 0;
+    for (int i = A.size() - 1; i >= 0; --i) {
+        // high bit to low bit
+        r = r * 10 + A[i];
+        C.push_back(r / b);
+        r %= b;
+    }
+    reverse(C.begin(), C.end());
     while (C.size() > 1 && C[C.size() - 1] == 0) C.pop_back();
     return C;
 }
